a02bd76cd03429a3eb1ee3a47d1c11b292cb27bd
RicardoGmPr <70128085+RicardoGmPr@users.noreply.github.com>
2022-05-31 11:46:31 -0700

    Ingreso de datos por usuario

--- EscrituraArchivos/EscrituraArchivos/Program.cs
+++ EscrituraArchivos/EscrituraArchivos/Program.cs
@@ -14,22 +14,30 @@ namespace EscrituraArchivos
             StreamWriter sw = new StreamWriter("ejemplo.txt", true);
             //Si el archivo no existe lo creará
             //Si ya existe, esxribirá en él
+            string Name;
+            int num;
+            Console.WriteLine("Cuantos nombres desea ingresar? ");
+            num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Digite el nombre que desea ingresar: ");
+                Name = Console.ReadLine();
+                string[] lines =
+                {
+                    Name
+                };
 
-            string[] lines =
-            {
-                "Esta es la nueva información de la primera línea",
-                "Esta es la segunda línea",
-                "Fin del texto"
-            };
-            //Recorrer el arreglo
-            foreach(string line in lines)
-            {
-                sw.WriteLine(line); //Escribe en el archivo
+                //Recorrer el arreglo
+                foreach (string line in lines)
+                {
+                    sw.WriteLine(line); //Escribe en el archivo
+                }
             }
-            sw.Close(); //Se debe cerrar el archivo
+                sw.Close(); //Se debe cerrar el archivo
 
-            Console.WriteLine("Escribiendo  en el archivo...");
-            Console.ReadKey();
+                Console.WriteLine("Escribiendo  en el archivo...");
+                Console.ReadKey();
+            
         }
     }
 }
